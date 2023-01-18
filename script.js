0885f2b85d208006b316772120f87db227ed49b7
BrianCodez <rainesbrian2@gmail.com>
2023-01-18 11:18:48 -0500

    grid with mouseover added

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //const div = document.createElement('div');
 
 const container = document.getElementById('grid-container');
+let mouseDown = true;
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
 
 
 function makeGrid(rows, cols){
@@ -8,10 +12,16 @@ function makeGrid(rows, cols){
     container.style.setProperty("--grid-cols",cols);
     for(c = 0; c < (rows * cols); c++){
         let cell = document.createElement("div");
-        cell.innerHTML = (c + 1);
-        container.appendChild(cell).className = "grid-item";
+        cell.classList.add("grid-item")
+        cell.onmouseover = function(){
+            cell.innerHTML = (c + 1);
+            return;
+            }
+        cell.classList.add("grid-item")
+        container.appendChild(cell)
     }
 }
+
 
 makeGrid(16,16);
  
